6efdc9560e20174b1e35ac522e7eb87f4fb53a3e
pmembari <parham.membari@terradue.com>
2025-08-19 17:42:48 +0200

    feat: update cwl files with the latest releases
    ESAEOEPCA-316

--- ogc-api-processes-with-zoo/files/init.sh
+++ ogc-api-processes-with-zoo/files/init.sh
@@ -1,7 +1,8 @@
 cd /workspace
+    owner=parham-membari-terradue
 
-    git clone https://github.com/eoap/ogc-api-processes-with-zoo.git
-   
+    git clone https://github.com/$owner/ogc-api-processes-with-zoo.git
+    
     code-server --install-extension ms-python.python 
 
     code-server --install-extension ms-toolsai.jupyter
@@ -29,3 +30,8 @@ cd /workspace
     export AWS_SECRET_ACCESS_KEY="test"
     
     aws s3 mb s3://results --endpoint-url=http://eoap-zoo-project-localstack:4566
+    
+    VERSION=$(curl -s https://api.github.com/repos/$owner/ogc-api-processes-with-zoo/releases/latest | jq -r '.tag_name')
+    curl -L -o "/workspace/ogc-api-processes-with-zoo/cwl-workflows/eoap-api-cli.cwl" \
+        "https://github.com/$owner/ogc-api-processes-with-zoo/releases/download/${VERSION}/eoap-api-cli.${VERSION}.cwl"
+    #rm /workspace/cwl-workflows/eoap-api-cli.cwl
