afa4c7a08908e78d1bbb9a0d0505c7f9a43eaa45
pmembari <parham.membari@terradue.com>
2025-09-02 15:18:53 +0200

    fix: change to no owner
    ESAEOEPCA-316

--- ogc-api-processes-with-zoo/files/init.sh
+++ ogc-api-processes-with-zoo/files/init.sh
@@ -1,7 +1,7 @@
 cd /workspace
-    OWNER=parham-membari-terradue
+    
 
-    git clone https://github.com/$OWNER/ogc-api-processes-with-zoo.git
+    git clone https://github.com/parham-membari-terradue/ogc-api-processes-with-zoo.git
     
     code-server --install-extension ms-python.python 
 
@@ -39,11 +39,11 @@ cd /workspace
     export AWS_DEFAULT_REGION="us-east-1"
     export AWS_ACCESS_KEY_ID="test"
     export AWS_SECRET_ACCESS_KEY="test"
-    export REPOSITORY_OWNER=$OWNER
+
 
     aws s3 mb s3://results --endpoint-url=http://eoap-zoo-project-localstack:4566
     
-    VERSION=$(curl -s https://api.github.com/repos/$OWNER/ogc-api-processes-with-zoo/releases/latest | jq -r '.tag_name')
+    VERSION=$(curl -s https://api.github.com/repos/parham-membari-terradue/ogc-api-processes-with-zoo/releases/latest | jq -r '.tag_name')
     curl -L -o "/workspace/ogc-api-processes-with-zoo/cwl-workflows/eoap-api-cli.cwl" \
-        "https://github.com/$OWNER/ogc-api-processes-with-zoo/releases/download/${VERSION}/eoap-api-cli.${VERSION}.cwl"
+        "https://github.com/parham-membari-terradue/ogc-api-processes-with-zoo/releases/download/${VERSION}/eoap-api-cli.${VERSION}.cwl"
     #rm /workspace/cwl-workflows/eoap-api-cli.cwl
